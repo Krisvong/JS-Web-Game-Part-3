d95a885dc43ef4632f5c040c3af0fddc73126ebc
Kristen von Gerichten <kristenvongerichten@Kristens-MBP.lan>
2022-12-18 14:31:02 -0500

    updated activity withclass review

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-function newImage(url, left, bottom){
+function newImage(url){
     let image = document.createElement('img')
     image.src = url
-    image.style.position = 'fixed'
-    image.style.left = left + 'px'
-    image.style.bottom = bottom + 'px'
+    // image.style.position = 'fixed'
+    // image.style.left = left + 'px'
+    // image.style.bottom = bottom + 'px'
     document.body.append(image)
     return image
 }
 
-newImage('assets/green-character.gif', 100, 250)
-newImage('assets/tree.png', 200, 450)
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
-newImage('assets/well.png', 500, 575)
-
-
-function newItem(url, left, bottom){
-    let item = newImage(url, left, bottom)
+function newItem(url){
+    let item = newImage(url)
     item.addEventListener('click', () => {
         item.remove()
         let inventoryItem = document.createElement('img')
@@ -48,4 +40,29 @@ function newInventory(){
     return inventory
 }
 
+function move (image){
+    image.style.position = 'fixed'
+
+    function moveToCoordinates(left, bottom){
+    image.style.left = left + 'px'
+    image.style.bottom = bottom + 'px'
+    }
+    return {
+    to: moveToCoordinates
+}
+}
+// let thingThatMoveReturns = move(greenCharacter)
+// thingThatMoveReturns.to
+// thingThatMoveReturns.to(300, 300)
+
 const inventory = newInventory()
+move(inventory).to(0, 0)
+move(newImage('assets/green-character.gif')).to(100, 250)
+move(newImage('assets/tree.png')).to(200, 450)
+move(newImage('assets/pillar.png')).to(350, 250)
+move(newImgae('assets/pine-tree.png')).to(450, 350)
+move(newImgage('assets/crate.png')).to(150, 350)
+move(newImage('assets/well.png')).to(500, 575)
+move(newImage('assets/sword')).to(500, 555)
+move(newImgae('assets/shield.png')).to(165, 335)
+move(newItem('assets/staff.png')).to(600, 250)
